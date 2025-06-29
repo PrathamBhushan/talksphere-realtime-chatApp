b3498b5fc41190760a1bb10df48fee0ad509d31a
Pratham Bhushan <prathambhushan04@gmail.com>
2025-06-29 22:42:30 +0530

    updated the limit of image size

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,6 +16,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -25,18 +28,15 @@ app.use(
   })
 );
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(clientDist, "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-  
 }
 
 server.listen(PORT, () => {
