7c42a889403a8614b2791acaaee88a54edb748bd
Pratham Bhushan <prathambhushan04@gmail.com>
2025-06-29 16:41:31 +0530

    fix: node version and wildcard route for Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,8 +13,6 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-console.log("✅ Loaded updated index.js without /: route");
-
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -35,9 +33,10 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
+  
 }
 
 server.listen(PORT, () => {
